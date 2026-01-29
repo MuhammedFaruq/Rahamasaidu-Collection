a8c0321406c7a31ca2118ec1ad04c6bf0688eb55
MuhammedFaruq <lawalmohammed389@gmail.com>
2026-01-29 17:46:00 +0100

    new code updated

--- script.js
+++ script.js
@@ -20,9 +20,12 @@
     let product = button.parentElement.querySelector("h3").textContent;
     let price = button.parentElement.querySelector(".price").textContent;
 
-    let message = `Hello AJ Walter Beauty! I am interested in the "${product}" `;
+    let message = `Hello Rahama Saidu Collection! I am interested in the "${product}" `;
 
-    let url = `https://wa.me/2349033847211?text=${encodeURIComponent(message)}`;
+    let url = `https://wa.me/2348088601874?text=${encodeURIComponent(message)}`;
 
     window.open(url, "_blank");
   }
+
+
+
